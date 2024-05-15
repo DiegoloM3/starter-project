bae9e8bd0d59d28c6c7b6cf99d134788b60933fc
lein-adq <carlosdaniel.quispepaye@gmail.com>
2024-05-15 06:57:00 -0400

    feat: Updating Bloc article files

--- frontend/lib/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart
+++ frontend/lib/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart
@@ -1,16 +1,22 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app_clean_architecture/core/resources/data_state.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/usecases/add_article_to_firestore.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/usecases/get_article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/usecases/get_firestore_article.dart';
 import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
 import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
 
 class RemoteArticlesBloc
     extends Bloc<RemoteArticlesEvent, RemoteArticlesState> {
   final GetArticleUseCase _getArticleUseCase;
+  final AddArticleToFirestoreUseCase _addArticleToFirestoreUseCase;
+  final GetArticlesFromFirestoreUseCase _getArticlesFromFirestoreUseCase;
 
-  RemoteArticlesBloc(this._getArticleUseCase)
+  RemoteArticlesBloc(this._getArticleUseCase,
+      this._addArticleToFirestoreUseCase, this._getArticlesFromFirestoreUseCase)
       : super(const RemoteArticlesLoading()) {
     on<GetArticles>(onGetArticles);
+    on<AddArticle>(onAddArticle);
   }
 
   void onGetArticles(
@@ -23,6 +29,20 @@ class RemoteArticlesBloc
 
     if (dataState is DataFailed) {
       emit(RemoteArticlesError(dataState.exception!));
+    }
+  }
+
+  void onAddArticle(
+      AddArticle addArticle, Emitter<RemoteArticlesState> emit) async {
+    await _addArticleToFirestoreUseCase(params: addArticle.article);
+    final dataState = await _getArticlesFromFirestoreUseCase();
+
+    if (dataState is DataSuccess) {
+      emit(RemoteArticlesDone(dataState.data!));
+    }
+
+    if (dataState is FireFailed) {
+      emit(FireArticlesError(dataState.fireException!));
     }
   }
 }
--- frontend/lib/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart
+++ frontend/lib/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart
@@ -1,12 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
+
 import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
 
 abstract class RemoteArticlesState extends Equatable {
   final List<ArticleEntity>? articles;
   final DioException? exception;
+  final FirebaseException? firebaseException;
 
-  const RemoteArticlesState({this.articles, this.exception});
+  const RemoteArticlesState(
+      {this.articles, this.exception, this.firebaseException});
 
   @override
   List<Object> get props => [articles!, exception!];
@@ -24,4 +28,9 @@ class RemoteArticlesDone extends RemoteArticlesState {
 class RemoteArticlesError extends RemoteArticlesState {
   const RemoteArticlesError(DioException exception)
       : super(exception: exception);
+}
+
+class FireArticlesError extends RemoteArticlesState {
+  const FireArticlesError(FirebaseException firebaseException)
+      : super(firebaseException: firebaseException);
 }
