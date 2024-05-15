a8096591cb74d7644a10bd30b31b2efb34b7b820
lein-adq <carlosdaniel.quispepaye@gmail.com>
2024-05-15 06:53:22 -0400

    feat: Adding new use cases for Firestore

---
+++ frontend/lib/features/daily_news/domain/usecases/add_article_to_firestore.dart
@@ -0,0 +1,14 @@
+import 'package:news_app_clean_architecture/core/usecase/usecase.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/repository/article_repository.dart';
+
+class AddArticleToFirestoreUseCase implements UseCase<void, ArticleEntity> {
+  final ArticleRepository _articleRepository;
+
+  AddArticleToFirestoreUseCase(this._articleRepository);
+
+  @override
+  Future<void> call({ArticleEntity? params}) {
+    return _articleRepository.addArticleToFirestore(params!);
+  }
+}
